d36015e012697ccc7b6c32fce1388d96772792af
jacktools <jacktoolsnet@gmail.com>
2024-04-21 09:37:59 +0200

    Echo send

--- backend/routes/echo.js
+++ backend/routes/echo.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 module.exports = function(app) {
     app.post('/echo', [security.checkToken, bodyParser.json], function(req, res) {
       res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify(req.body, null, 2));
+      res.setStatus(200)
+      res.send(JSON.stringify(req.body, null, 2));
     });
   };
