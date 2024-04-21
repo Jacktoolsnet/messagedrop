f14d344d3fe5f15501a2684ce6bfd0f0f7bf3257
jacktools <jacktoolsnet@gmail.com>
2024-04-21 09:45:47 +0200

    Echo bugfix

--- backend/routes/echo.js
+++ backend/routes/echo.js
@@ -5,6 +5,6 @@ module.exports = function(app) {
     app.post('/echo', [security.checkToken, bodyParser.json], function(req, res) {
       res.setHeader('Content-Type', 'application/json');
       res.status(200)
-      res.end(JSON.stringify(req.body, null, 2));
+      res.send(JSON.stringify(req.body, null, 2));
     });
   };
