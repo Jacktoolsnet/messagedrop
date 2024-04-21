6423d4f52f60100c8e89c54c53efba61b08db4b0
jacktools <jacktoolsnet@gmail.com>
2024-04-21 09:51:04 +0200

    Echo test

--- backend/routes/echo.js
+++ backend/routes/echo.js
@@ -2,7 +2,7 @@ const security = require('../middleware/security');
 const bodyParser = require('body-parser');
 
 module.exports = function(app) {
-    app.post('/echo', [security.checkToken, bodyParser.json], function(req, res) {
+    app.get('/echo', [security.checkToken, bodyParser.json], function(req, res) {
       res.setHeader('Content-Type', 'application/json');
       res.status(200);
       res.send(JSON.stringify(req.body, null, 2));
