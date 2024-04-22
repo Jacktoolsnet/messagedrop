ece05033f2b3e5168d70bcae018110e0b64748bc
jacktools <jacktoolsnet@gmail.com>
2024-04-22 20:18:23 +0200

    db is working

--- backend/routes/root.js
+++ backend/routes/root.js
@@ -1,7 +1,5 @@
-const path = require('path');
-
 module.exports = function(app) {
     app.get('/', function(req, res) {
-      res.send("Service is up and running. " + path.dirname(__filename));
+      res.send("Service is up and running.");
     });
   };
