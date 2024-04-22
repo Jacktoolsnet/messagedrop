3c87596138ce0c203980dc4c60033e1a9d68b7e5
jacktools <jacktoolsnet@gmail.com>
2024-04-22 20:15:40 +0200

    test

--- backend/routes/root.js
+++ backend/routes/root.js
@@ -1,5 +1,7 @@
+const path = require('path');
+
 module.exports = function(app) {
     app.get('/', function(req, res) {
-      res.send("Service is up and running.");
+      res.send("Service is up and running. " + path.dirname(__filename));
     });
   };
