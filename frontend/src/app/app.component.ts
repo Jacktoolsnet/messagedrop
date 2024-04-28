8e4169e5022a34330a49d537d6fdabbcfc59bbda
jacktools <jacktoolsnet@gmail.com>
2024-04-28 21:05:48 +0200

    Map and geolocation

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from './../environments/environment';
+import { MapComponent } from './map/map.component';
+import { GeolocationService } from './services/geolocation.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  private title: String = 'frontend';
+  private apiUrl: String = environment.apiUrl;
+  public latitude: number = 0;
+  public longitude: number = 0;
+
+  constructor(private geolocationService: GeolocationService) { }
+
+  ngOnInit(): void {
+    this.getGeoLocation();
+  }
+
+  getGeoLocation() {
+    this.geolocationService.getCurrentPosition().subscribe({
+      next: (position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+      },
+      error: (error) => {
+        console.error('Error getting geolocation:', error);
+      },
+    });
+  }
 }
---
+++ frontend/src/app/map/map.component.ts
@@ -0,0 +1,61 @@
+import { Component, AfterViewInit , Input, OnChanges, SimpleChanges} from '@angular/core';
+import * as leaflet from 'leaflet';
+
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = leaflet.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+leaflet.Marker.prototype.options.icon = iconDefault;
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.css'
+})
+export class MapComponent implements AfterViewInit, OnChanges {
+  @Input() latitude: number = 0;
+  @Input() longitude: number = 0;
+  @Input() zoom: number = 18;
+  private map: any;
+  private marker: any;
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.map.setView(new leaflet.LatLng(this.latitude, this.longitude), this.zoom);
+    this.marker.setLatLng([this.latitude, this.longitude]).update();
+  }
+
+  private initMap(): void {
+    this.map = leaflet.map('map', {
+      center: [ this.latitude, this.longitude ],
+      zoom: this.zoom
+    });
+
+    const tiles = leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+
+    this.marker = leaflet.marker([this.latitude, this.longitude]).addTo(this.map);
+  }
+  
+  constructor() { 
+  }
+
+  ngAfterViewInit(): void { 
+    this.initMap();
+  }
+}
