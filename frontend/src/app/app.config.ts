9819ce293570b336d2e497766340ec5144f72206
jacktools <jacktoolsnet@gmail.com>
2024-05-26 19:38:50 +0200

    Update

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,7 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideServiceWorker } from '@angular/service-worker';
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
