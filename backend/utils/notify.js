a7c5f77c6a3ec58bea218fc629115040c7454502
jacktools <jacktoolsnet@gmail.com>
2024-08-13 16:34:38 +0200

    Included web push

--- backend/utils/notify.js
+++ backend/utils/notify.js
@@ -1,3 +1,5 @@
+const webpush = require('web-push');
+
 const placeSubscriptions = function (db, plusCode, userId) {
     try{
         let sql = `
@@ -7,16 +9,44 @@ const placeSubscriptions = function (db, plusCode, userId) {
         INNER JOIN tableUser ON tablePlace.userId = tableUser.id
         WHERE plusCode = '${plusCode}'
         AND subscribed = 1
-        AND tablePlace.userID <> '${userId}';`;
-        console.log(sql);
+        AND tablePlace.userID = '${userId}';`;
         db.all(sql, (err, rows) => {
             rows.forEach((row) => {
-                console.log(row);
+                const payload = {
+                    "notification": {
+                        "title": "New message dropped",
+                        "body": "Newsletter Available!",
+                        "icon": "assets/marker/messages-marker.svg",
+                        "vibrate": [100, 50, 100],
+                        "data": {
+                            "dateOfArrival": Date.now(),
+                            "primaryKey": row.plusCode
+                        },
+                        "actions": [{
+                            "action": "explore",
+                            "title": "Go to the location"
+                        }]
+                    }
+                };
+                sendNotification(JSON.parse(row.subscription), payload)
               });
         });
     } catch (error) {
         throw error;
     }
+}
+
+function sendNotification(subscription, payload) {
+    webpush.setVapidDetails(
+        'https://messagedrop.de',
+        process.env.VAPID_PUBLIC_KEY,
+        process.env.VAPID_PRIVATE_KEY
+    );
+    webPush
+        .sendNotification(subscription, payload)
+        .then(() => {})
+        .catch((error) => {
+        });
 }
 
 module.exports = {
