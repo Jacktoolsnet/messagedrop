58ab5e38be1748f86519676ecb1a7488927c60ed
jacktools <jacktoolsnet@gmail.com>
2025-04-21 12:23:01 +0200

    Optimisations

--- frontend/src/app/components/utils/server-down/server-down.component.ts
+++ frontend/src/app/components/utils/server-down/server-down.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-server-down',
   imports: [
+    CommonModule,
     MatButtonModule
   ],
   templateUrl: './server-down.component.html',
   styleUrl: './server-down.component.css'
 })
-export class ServerDownComponent {
+export class ServerDownComponent implements OnInit {
+  public showRetry = false;
+
   constructor(public dialogRef: MatDialogRef<ServerDownComponent>) { }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.showRetry = true;
+    }, 10000);
+  }
 }
