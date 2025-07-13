51cfb2800b7ba766af0a903e3297a6934ca4deb2
jacktools <jacktoolsnet@gmail.com>
2025-07-13 21:50:51 +0200

    changed qrcode package

--- frontend/src/app/components/utils/qrcode/qrcode.component.ts
+++ frontend/src/app/components/utils/qrcode/qrcode.component.ts
@@ -1,20 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { Component, Inject } from '@angular/core';
+import { Component, ElementRef, Inject, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogContent } from '@angular/material/dialog';
-import { QRCodeComponent } from 'angularx-qrcode';
+import QRCode from 'qrcode';
 
 @Component({
   selector: 'app-qrcode',
   imports: [
     MatDialogContent,
-    CommonModule,
-    QRCodeComponent
+    CommonModule
   ],
   templateUrl: './qrcode.component.html',
   styleUrl: './qrcode.component.css'
 })
 export class QrcodeComponent {
+  @ViewChild('canvas', { static: true }) canvasRef!: ElementRef<HTMLCanvasElement>;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: { qrData: string }) { }
+
+  ngOnInit(): void {
+    QRCode.toCanvas(this.canvasRef.nativeElement, this.data.qrData, {
+      width: 256,
+      errorCorrectionLevel: 'M'
+    }, (error: any) => {
+      if (error) console.error(error);
+    });
+  }
 
 }
