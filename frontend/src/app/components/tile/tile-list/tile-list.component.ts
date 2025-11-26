28baaff0e654a01a5182b09f996a58e72e5c3cde
jacktools <jacktoolsnet@gmail.com>
2025-11-26 17:17:04 +0100

    Layout progress

--- frontend/src/app/components/tile/tile-list/tile-list.component.ts
+++ frontend/src/app/components/tile/tile-list/tile-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Place } from '../../../interfaces/place';
 import { WeatherTileComponent } from "../../placelist/weather-tile/weather-tile.component";
 import { AirQualityTileComponent } from "../air-quality-tile/air-quality-tile.component";
@@ -6,12 +6,14 @@ import { DateTimeTileComponent } from "../datetime-tile/datetime-tile.component"
 import { ImageTileComponent } from "../image-tile/image-tile.component";
 import { MessageTileComponent } from "../message-tile/messagetile.component";
 import { NoteTileComponent } from "../note-tile/note-tile.component";
+import { MasonryItemDirective } from "../../../directives/masonry-item.directive";
 
 @Component({
   selector: 'app-tile-list',
-  imports: [DateTimeTileComponent, WeatherTileComponent, AirQualityTileComponent, NoteTileComponent, MessageTileComponent, ImageTileComponent],
+  imports: [DateTimeTileComponent, WeatherTileComponent, AirQualityTileComponent, NoteTileComponent, MessageTileComponent, ImageTileComponent, MasonryItemDirective],
   templateUrl: './tile-list.component.html',
-  styleUrl: './tile-list.component.css'
+  styleUrl: './tile-list.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TileListComponent {
   @Input() place!: Place;
