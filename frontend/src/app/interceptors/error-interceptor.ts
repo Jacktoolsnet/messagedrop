b7695a35bb7dcb4bb78920268228f986e52091d6
jacktools <jacktoolsnet@gmail.com>
2025-12-27 17:33:07 +0100

    transloco

--- frontend/src/app/interceptors/error-interceptor.ts
+++ frontend/src/app/interceptors/error-interceptor.ts
@@ -1,4 +1,4 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { catchError, throwError } from 'rxjs';
@@ -10,6 +10,14 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
 
   return next(req).pipe(
     catchError((error: unknown) => {
+      if (
+        error instanceof HttpErrorResponse &&
+        error.status === 404 &&
+        (req.url.includes('/message/get/boundingbox') ||
+          req.url.includes('/place/get/userId'))
+      ) {
+        return throwError(() => error);
+      }
       const message = apiErrorService.getErrorMessage(error);
       snackBar.open(message, undefined, {
         duration: 4000,
