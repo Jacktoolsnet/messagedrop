fed3eb170d995c90d541d42561dff8759e45a840
jacktools <jacktoolsnet@gmail.com>
2026-01-05 06:37:57 +0100

    layout

--- frontend/src/app/components/utils/text/text.component.ts
+++ frontend/src/app/components/utils/text/text.component.ts
@@ -2,11 +2,18 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MAT_DIALOG_DATA, MatDialogActions, MatDialogContent, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogActions, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIcon } from "@angular/material/icon";
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
+import { TranslocoPipe } from '@jsverse/transloco';
+
+type TextDialogData = {
+  text: string;
+  titleKey?: string;
+  titleIcon?: string;
+};
 
 @Component({
   selector: 'app-message',
@@ -14,20 +21,28 @@ import { MatMenuModule } from '@angular/material/menu';
     MatButtonModule,
     MatDialogActions,
     MatDialogContent,
+    MatDialogTitle,
     FormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatMenuModule,
-    MatIcon
+    MatIcon,
+    TranslocoPipe
   ],
   templateUrl: './text.component.html',
   styleUrl: './text.component.css'
 })
 export class TextComponent {
   readonly dialogRef = inject(MatDialogRef<TextComponent>);
-  readonly data = inject<{ text: string }>(MAT_DIALOG_DATA);
+  readonly data = inject<TextDialogData>(MAT_DIALOG_DATA);
+  readonly titleKey = this.data.titleKey ?? 'common.textDialog.editTitle';
+  readonly titleIcon = this.data.titleIcon ?? 'edit';
 
   onApplyClick(): void {
     this.dialogRef.close(this.data);
+  }
+
+  onAbortClick(): void {
+    this.dialogRef.close();
   }
 }
