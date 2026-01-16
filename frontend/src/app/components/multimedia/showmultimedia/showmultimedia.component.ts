a6dde98802ed9bfc295a89ede0e7d21ca0f77406
jacktools <jacktoolsnet@gmail.com>
2026-01-16 21:21:24 +0100

    Optimisations

--- frontend/src/app/components/multimedia/showmultimedia/showmultimedia.component.ts
+++ frontend/src/app/components/multimedia/showmultimedia/showmultimedia.component.ts
@@ -1,18 +1,19 @@
 
-import { Component, Input, OnChanges, SimpleChanges, inject } from '@angular/core';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { Component, Input, OnChanges, SimpleChanges, effect, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
 import { DomSanitizer, SafeHtml, SafeResourceUrl } from '@angular/platform-browser';
 import { TranslocoPipe } from '@jsverse/transloco';
 import { Multimedia } from '../../../interfaces/multimedia';
 import { AppService } from '../../../services/app.service';
 import { OembedService } from '../../../services/oembed.service';
-import { EnableExternalContentComponent } from '../../utils/enable-external-content/enable-external-content.component';
+import { TranslationHelperService } from '../../../services/translation-helper.service';
+import { ExternalContentComponent } from '../../legal/external-content/external-content.component';
 
 @Component({
   selector: 'app-showmultimedia',
   imports: [
-    MatSlideToggleModule,
-    EnableExternalContentComponent,
+    MatButtonModule,
     TranslocoPipe
 ],
   templateUrl: './showmultimedia.component.html',
@@ -28,10 +29,18 @@ export class ShowmultimediaComponent implements OnChanges {
 
   // Activation logic
   isPlatformEnabled = false;
+  disabledReason = '';
+  showExternalSettingsButton = false;
 
   private readonly sanitizer = inject(DomSanitizer);
+  private readonly dialog = inject(MatDialog);
   private readonly appService = inject(AppService);
   private readonly oembedService = inject(OembedService);
+  private readonly translation = inject(TranslationHelperService);
+  private readonly settingsEffect = effect(() => {
+    this.appService.settingsSet();
+    this.updateFromMultimedia();
+  });
 
   ngOnChanges(changes: SimpleChanges) {
     if ('multimedia' in changes) {
@@ -41,6 +50,8 @@ export class ShowmultimediaComponent implements OnChanges {
 
   private updateFromMultimedia(): void {
     const settings = this.appService.getAppSettings();
+    this.disabledReason = '';
+    this.showExternalSettingsButton = false;
     switch (this.multimedia?.type) {
       case 'youtube':
         this.isPlatformEnabled = settings.enableYoutubeContent;
@@ -65,18 +76,33 @@ export class ShowmultimediaComponent implements OnChanges {
         break;
     }
 
+    if (this.multimedia?.type && !this.isPlatformEnabled) {
+      this.disabledReason = this.translation.t('common.multimedia.platformDisabled', {
+        platform: this.getPlatformLabel(this.multimedia.type)
+      });
+      this.showExternalSettingsButton = true;
+      this.safeHtml = undefined;
+      return;
+    }
+
     this.safeHtml = this.isPlatformEnabled && this.isOembedAllowed()
       ? this.sanitizer.bypassSecurityTrustHtml(this.multimedia?.oembed?.html ?? '')
       : undefined;
   }
 
-  onEnabledChange(enabled: boolean) {
-    this.isPlatformEnabled = enabled;
-    if (enabled) {
-      this.safeHtml = this.isOembedAllowed()
-        ? this.sanitizer.bypassSecurityTrustHtml(this.multimedia?.oembed?.html ?? '')
-        : undefined;
-    }
+  openExternalContentSettings(): void {
+    const dialogRef = this.dialog.open(ExternalContentComponent, {
+      data: { appSettings: this.appService.getAppSettings() },
+      maxWidth: '90vw',
+      maxHeight: '90vh',
+      width: 'min(700px, 90vw)',
+      height: 'auto',
+      autoFocus: false
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      this.updateFromMultimedia();
+    });
   }
 
   private isOembedAllowed(): boolean {
@@ -84,6 +110,25 @@ export class ShowmultimediaComponent implements OnChanges {
       this.multimedia?.sourceUrl,
       this.multimedia?.oembed?.provider_url
     );
+  }
+
+  private getPlatformLabel(type: string): string {
+    switch (type) {
+      case 'youtube':
+        return 'YouTube';
+      case 'spotify':
+        return 'Spotify';
+      case 'tiktok':
+        return 'TikTok';
+      case 'pinterest':
+        return 'Pinterest';
+      case 'tenor':
+        return 'Tenor';
+      case 'unsplash':
+        return 'Unsplash';
+      default:
+        return this.translation.t('common.legal.externalContent.title');
+    }
   }
 
 }
