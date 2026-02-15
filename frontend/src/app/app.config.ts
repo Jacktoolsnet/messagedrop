9ea302f9e86638daca82bcd5b27626fe3789f88f
jacktools <jacktoolsnet@gmail.com>
2026-02-15 09:18:09 +0100

    optimisations

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -51,6 +51,6 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
-      registrationStrategy: 'registerWhenStable:30000'
+      registrationStrategy: 'registerImmediately'
     })]
 };
